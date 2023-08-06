7822b3bb61a517c2ee4d78fdb91a7e45878cab3a
ShahabAthar25 <shahabathar25@gmail.com>
2023-08-06 16:40:28 +0500

    Add boiler plate

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const app = express();
+const PORT = 5000 || process.env.PORT;
+
+dotenv.config();
+
+app.use(express.json({ limit: "50kb" }));
+app.use(express.urlencoded({ limit: "50kb", extended: true }));
+app.use(morgan("common"));
+app.use(helmet());
+app.use(
+  cors({
+    exposedHeaders: "Authorization",
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
