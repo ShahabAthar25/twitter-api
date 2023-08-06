8b0eaa7b03954b8edeec9869f303983566bf1fd3
ShahabAthar25 <shahabathar25@gmail.com>
2023-08-06 17:59:59 +0500

    Add generate Access token method

---
+++ utils/generateAccessToken.js
@@ -0,0 +1,10 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+module.exports = (payload) => {
+  return jwt.sign(payload, process.env.PRIVATE_KEY, {
+    expiresIn: "15m",
+  });
+};
